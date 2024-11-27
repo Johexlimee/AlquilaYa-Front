daf6a6b0b1432474dbbba81786cd0c386384e193
Johexlimee <jhcastillo@uniempresarial.edu.co>
2024-11-27 15:56:45 -0500

    Cositas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { ListProductsComponent } from './list-products/list-products.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    HeaderComponent,
+    ListProductsComponent,
+    DetailProductComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
