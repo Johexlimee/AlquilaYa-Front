b5075682c82f5acd9d7db44c776349c4da75fb08
Johexlimee <jhcastillo@uniempresarial.edu.co>
2024-11-29 19:20:43 -0500

    Cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListProductsComponent } from './list-products/list-products.component';
+import { HomeComponent } from './home/home.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
-  { path: 'listproducts', component: ListProductsComponent }
+  { path: '', component: HomeComponent },
+  { path: 'listproducts', component: ListProductsComponent },
+  { path: 'detailproduct', component: DetailProductComponent },
+  { path: 'cart', component: CartComponent},
 ];
 
 @NgModule({
