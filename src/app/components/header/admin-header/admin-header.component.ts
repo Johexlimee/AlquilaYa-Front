a92b7a58ad332be248b242384165d4b6f472360d
liceth1006 <olmos.1006@gmail.com>
2024-12-02 15:12:35 -0500

    sidebar emit

--- src/app/components/header/admin-header/admin-header.component.ts
+++ src/app/components/header/admin-header/admin-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-admin-header',
@@ -6,8 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './admin-header.component.css'
 })
 export class AdminHeaderComponent {
-  toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar')!;
-    sidebar.classList.toggle('collapsed');
+  @Input() isSidebarCollapsed: boolean = false; // Recibe el estado del sidebar desde el padre
+  @Output() toggleSidebar = new EventEmitter<void>(); // Emite eventos al padre
+
+  // Método para emitir el evento
+  onToggleSidebar() {
+    this.toggleSidebar.emit();
   }
+
+ 
 }
