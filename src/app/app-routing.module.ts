a6b682bbd80f942d674b91c33e6edf3254c01202
liceth1006 <olmos.1006@gmail.com>
2024-12-06 23:14:25 -0500

    guard router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { authGuard  } from './auth.guard';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./guest/guest.module').then(m => m.GuestModule) },
-  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m=>m.AdminModule) }
+  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule), canActivate: [authGuard] },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m=>m.AdminModule), canActivate: [authGuard] }
 ];
 
 @NgModule({
